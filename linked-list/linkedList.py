edfd4377d1b4d400e962aa23aa314af0627742c8
tawhidii <barii.py@gmail.com>
2022-10-30 00:33:29 +0600

    Added LinkedList example

--- linked-list/linkedList.py
+++ linked-list/linkedList.py
@@ -1,15 +1,37 @@
+
+
 class Node:
-    def __init__(self,value):
-        self.value = value
+    def __init__(self,data):
+        self.data = data
         self.next = None
 
 class LinkedList:
-    def __init__(self,val):
+    def __init__(self,val=None):
         newNode = Node(val)
         self.head = newNode
         self.tail = newNode
         self.length = 1
 
+    def apppend(self,value):
+        newNode = Node(value)
+        if self.length == 0 or self.head.data is None:
+            self.head = newNode
+            self.tail = newNode
+        else: 
+            self.tail.next = newNode
+            self.tail = newNode
+        self.length += 1
 
 
-linkeList = LinkedList(70)
+    def show_list(self):
+        temp_var = self.head
+        while temp_var is not None:
+            print(temp_var.data)
+            temp_var = temp_var.next
+
+
+
+linkeList = LinkedList()
+linkeList.apppend(170)
+
+linkeList.show_list()
